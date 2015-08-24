8f2acad7ca40f8e7eb76aa8b0728ec680a0abb6d
unknown <paul-cot@PC-PCO.gestimum.local>
2015-08-24 18:20:43 +0200

    commit pour récupérr code sur github

--- src/interfaces/NeedSelectionFichiers.java
+++
@@ -1,9 +0,0 @@
-package interfaces;
-
-import java.io.File;
-import java.util.ArrayList;
-
-public interface NeedSelectionFichiers extends Runnable, LongTask {
-
-	public void fichiersSelectionnes(ArrayList<File> files);
-}
